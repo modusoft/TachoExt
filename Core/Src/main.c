aaaca84bf4b2e8e657546ea467142b0a1147992f
Sascha Ittner <sascha.ittner@modusoft.de>
2026-03-12 13:02:33 +0100

    reenabled SWD

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -439,9 +439,9 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA8 PA9 PA10 PA11
-                           PA12 PA13 PA14 */
+                           PA12 */
   GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11
-                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14;
+                          |GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
